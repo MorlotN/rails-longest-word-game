48f366b475a2b613a49286e6d9a9731857da4c94
Nicolas Morlot <nicolasmorlot@gmail.com>
2019-11-04 17:41:38 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'games/contact'
+  get 'games/new'
+  post 'games/score'
+
+  root to: 'games#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
